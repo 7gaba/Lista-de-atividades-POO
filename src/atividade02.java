4bf2bfb0e3c8f517710327abe0590283fb4f3279
7gaba <kauanschutzrabelo@gmail.com>
2025-08-11 22:44:14 -0300

    terceiro commit

---
+++ src/atividade02.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class atividade02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new scanner(System.in);
+		System.out.println("Digite um numero:");
+		int numero = sc.nextInt();
+		System.out.println("O numero digitado foi: " + numero);
+		sc.close();
+	}
+
+}
---
+++ src/lista01_atividade01.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class lista01_atividade01 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+
+}
